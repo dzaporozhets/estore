dacd27ac0f610d8c146d5b0ada67569c43ed7de2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-08-01 15:52:38 +0300

    Add product scaffold
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(
+  title: 'Star Wars: Episode VI - Return of the Jedi',
+  description: 'After a daring mission to rescue Han Solo from Jabba the Hutt, the rebels dispatch to Endor to destroy a more powerful Death Star. Meanwhile, Luke struggles to help Vader back from the dark side without falling into the Emperors trap.',
+  price: 13.99
+)
+
+
+Product.create(
+  title: 'Star Wars: Episode V - The Empire Strikes Back',
+  description: 'After the rebels are overpowered by the Empire on their newly established base, Luke Skywalker begins Jedi training with Master Yoda. His friends accept shelter from a questionable ally as Darth Vader hunts them in a plan to capture Luke.',
+  price: 11.99
+)
